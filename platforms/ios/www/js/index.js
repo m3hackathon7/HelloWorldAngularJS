27d3434137235e2028e1575e4dbf3dd857ba2262
namutaka <namutaka+g@gmail.com>
2014-10-13 20:35:03 +0900

    ビルドによる生成・変化物

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,51 +1,36 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+// index.js
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+console.log('index.js load');
 
-        console.log('Received Event: ' + id);
-    }
-};
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOMContentLoadedイベントが発火しました');
+}, false);
 
-app.initialize();
+document.addEventListener('load', function () {
+  console.log('loadイベントが発火しました');
+}, false);
+
+
+document.addEventListener('deviceready', function () {
+  console.log('devicereadyイベントが発火しました');
+}, false);
+
+
+var app = angular.module("hello", []);
+
+app.controller("IndexController", ['$timeout', function($timeout) {
+  var self = this;
+
+  document.addEventListener('deviceready', function() {
+    console.log('deviceready event in angularjs');
+    $timeout(function() {
+
+      self.received = true;
+
+    }, 0);
+  }, false);
+
+  console.log('angularjs controller');
+}]);
+
+
